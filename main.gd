5d6f3bbd5131d1724f775126e177f3ab7292c489
ormenachem <orchickul@gmail.com>
2022-10-06 16:27:18 +0300

    removed darkness

--- main.gd
+++ main.gd
@@ -2,10 +2,3 @@ extends Node2D
 onready var darkness = $darkness
 var dark = false
 
-func _physics_process(delta):
-	darkness.visible = dark
-	if Input.is_action_just_pressed("jump") and get_node("Player").is_on_floor():
-		if dark == false:
-			dark = true
-		else:
-			dark = false

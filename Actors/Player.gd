889dd99f4129c5aaa2d79be40d0b6f8fd4f8d437
hedgein <hedgejess@gmail.com>
2022-10-06 08:23:35 -0500

    streamlined left/right movement to be celeste-like haha

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
 # the movement variables (mess around with them to make the movement the way you like)
-export (int) var speed = 300
+export (int) var FACTOR = 5.5
+var MAX_SPEED = 90
+var FRICTION = 900 
+var ACCEL = 1000 
+#export (int) var speed_factor = 1.0
 export (int) var jump_speed = -600
 export (int) var gravity = 1000
 export (float, 0, 1.0) var friction = 0.25
@@ -12,21 +16,26 @@ var dark = false
 
 onready var darkness = $darkness
 # geting player input with our own function
-func get_input():
+func get_input(delta):
 	var dir = 0
 	if Input.is_action_pressed("move_right"):
 		dir += 1
 	if Input.is_action_pressed("move_left"):
 		dir -= 1
-	if dir != 0:
-		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	# 6 frame acceleration?? (Not sure if i remember 
+	# how to check this, but fingers crossed
+	#!((time_start + delta) > OS.get_unix_time())
+	if abs(velocity.x) >= MAX_SPEED * FACTOR and sign(velocity.x) == dir:
+		velocity.x = move_toward(velocity.x, dir * MAX_SPEED * FACTOR, FRICTION * FACTOR * delta)
 	else:
-		velocity.x = lerp(velocity.x, 0, friction)
+		velocity.x = move_toward(velocity.x, dir * MAX_SPEED * FACTOR , ACCEL * FACTOR * delta)
+#	else:
+#		velocity.x = lerp(velocity.x, 0, friction)
 
 # and finaly calculating the movement
 func _physics_process(delta):
 	
-	get_input()
+	get_input(delta)
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	if Input.is_action_just_pressed("move_up"):
